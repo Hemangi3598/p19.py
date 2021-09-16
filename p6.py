2564d61c41db3446beae5f0a80e265e3353ddddc
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-16 16:43:52 +0530

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,10 @@
+#wapp using 2 integers with assingment operator 
+a=87;b=2 ; print(a);print(b);
+a=87;b=2 ;  a+=b; print(a);print(b);
+a=87;b=2 ;  a-=b; print(a);print(b);
+a=87;b=2 ;  a/=b; print(a);print(b);
+a=87;b=2 ; a*=b; print(a);print(b);
+a=87;b=2 ; a//=b; print(a);print(b);
+a=87;b=2 ;  a**=b; print(a);print(b);
+a=87;b=2 ; a%=b; print(a);print(b);
+#here a+=b --> a=a+b 
